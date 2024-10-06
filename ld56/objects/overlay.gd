766da7884329bb91538c02c9c860866816332d16
Cradac <max.dettmann@web.de>
2024-10-06 13:14:28 +0200

    pointy finger

--- ld56/objects/overlay.gd
+++ ld56/objects/overlay.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var bottom_right_corner = Vector2(1920,1080)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.global_position = get_global_mouse_position()
-	pass
+	var future_position = get_global_mouse_position()
+	self.global_position = future_position
+	var angle = future_position.angle_to_point(bottom_right_corner)
+	self.set_global_rotation(angle)
