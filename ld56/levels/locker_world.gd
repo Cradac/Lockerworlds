c386ac26da2ba6163f0513d7420686bc9303e8e5
Phil <dev@cybermuell.rocks>
2024-10-06 11:33:59 +0200

    snooth operator

--- ld56/levels/locker_world.gd
+++ ld56/levels/locker_world.gd
@@ -5,6 +5,8 @@ var WORLD_2_LOCKER = null
 
 var locker_ids = []
 
+
+
 @onready var lockers = self.get_node("Lockers").get_children()
 @onready var post_its = self.get_node("PostIts").get_children()
 @onready var camera_pos = $Camera2D.global_transform
@@ -48,10 +50,15 @@ func select_action_lockers():
 func _on_locker_input_event(viewport: Node, event: InputEvent, shape_idx: int, index: int) -> void:
 	if event is InputEventMouseButton:
 		var evt = event as InputEventMouseButton
+		var tween = get_tree().create_tween()
 		if evt.button_index == 1 and evt.pressed:
 			if $Camera2D.zoom == Vector2(1,1):
 				$Camera2D.global_transform = lockers[index].get_child(0).global_transform
-				$Camera2D.zoom = Vector2(6,6)
+				tween.tween_property($Camera2D,"zoom",Vector2(6,6),1)
+				#$Camera2D.zoom =  $Camera2D.zoom.lerp(Vector2(6,6),0.6)
 			else:
-				$Camera2D.zoom = Vector2(1,1)
+				$Camera2D.zoom 
+				#$Camera2D.zoom = Vector2(1,1)
+				tween.tween_property($Camera2D,"zoom",Vector2(1,1),1)
+				#tween.tween_property($Camera2D,"global_transform",camera_pos,1)
 				$Camera2D.global_transform = camera_pos
